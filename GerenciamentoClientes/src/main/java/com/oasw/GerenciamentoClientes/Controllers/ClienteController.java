0588aca821bea1d3c6207c154aebe45fbbf55b4f
OtavioAugusto <santostatogusto@gmail.com>
2026-03-22 17:07:33 -0300

    controller criado

---
+++ GerenciamentoClientes/src/main/java/com/oasw/GerenciamentoClientes/Controllers/ClienteController.java
@@ -0,0 +1,44 @@
+package com.oasw.GerenciamentoClientes.Controllers;
+import com.oasw.GerenciamentoClientes.Models.ClienteModel;
+import com.oasw.GerenciamentoClientes.Services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/clientes")
+public class ClienteController {
+
+    @Autowired
+    ClienteService clienteService;
+
+    @PostMapping
+    public ResponseEntity<ClienteModel> create(ClienteModel clienteModel){
+        ClienteModel request = clienteService.create(clienteModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(clienteModel.getId()).toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteModel>> getAll(){
+        List<ClienteModel> request = clienteService.getAll();
+        return ResponseEntity.ok().body(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteModel> getById(@PathVariable Long id){
+        ClienteModel request = clienteService.getById(id);
+        return ResponseEntity.ok().body(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        clienteService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
